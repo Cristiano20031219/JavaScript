ab98f33ec77d5175d7cfbe1e503c90b3117a7bb5
Cristiano Mota <jcristianofilho52@gmail.com>
2023-03-15 02:23:47 -0300

    refactoring: instituição

--- aulas/media_aritmetica.js
+++ aulas/media_aritmetica.js
@@ -1,3 +1,4 @@
+var instituição = "instituto federal de alagoas"
 var nome = "Aluno"
 var materia = "Programação Web"
 var valores1 = 6
@@ -7,8 +8,8 @@ var valores4 = 5
 var media_aritmetica = (valores1+valores2+valores3+valores4)/4
 console.log(nome,"Sua nota final foi",media_aritmetica)
 if (media_aritmetica >= 6){
-    console.log("aluno aprovado na materia de",materia)
+    console.log("Aluno da instituição",instituição,"aluno aprovado na materia de",materia)
 }else if (media_aritmetica< 6){
-    console.log("aluno reprovado na materia de",materia)
+    console.log("Aluno da instituição",instituição,"aluno reprovado na materia de",materia)
 
 }
